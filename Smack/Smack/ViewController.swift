914b4b427f28ddb833435f989bae7677d48ff49e
Pooya <pooya@MacBook-Air.local>
2018-03-23 03:26:17 +0430

    Project setup is done.

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Pooya on 1/3/1397 AP.
+//  Copyright © 1397 Binogoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
